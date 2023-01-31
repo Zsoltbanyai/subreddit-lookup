3adc898f038e486af6ada42f5909534c80d93cdd
Zsoltbanyai <zsolt.banyai94@gmail.com>
2023-01-31 18:08:15 +0100

    setup frontend and backend structure

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from Express!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
